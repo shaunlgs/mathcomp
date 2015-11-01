8f22ba050c403cbf6a3e3f23ccef38fa4f9aae22
shaunling <linggensheng@hotmail.com>
2015-11-02 01:03:44 +0800

    doing wolfram alpha api call php

---
+++ wolfram.php
@@ -0,0 +1,30 @@
+<?php
+		function Parse ($url) {
+        $fileContents= file_get_contents($url);
+        $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+        $fileContents = trim(str_replace('"', "'", $fileContents));
+        $simpleXml = simplexml_load_string($fileContents);
+        $json = json_encode($simpleXml);
+        return $json;
+		}
+		
+		$input = urlencode($_POST['input']);
+		
+		if (isset($_POST['type'])){
+			$type = $_POST['type'];
+		}else{
+			$type = "json";
+		}
+		
+	if ($type == "json"){
+		$return["name"] = $_POST['input'];
+		$return["jsonCall"] = Parse("http://api.wolframalpha.com/v2/query?input=".$input."&appid=8XV52V-P6RE2RGYA9");
+		echo json_encode($return);
+		
+	}else if ($type == "xml"){
+		$return["name"] = $_POST['input'];
+		$return["xmlCall"] = file_get_contents("http://api.wolframalpha.com/v2/query?input=".$input."&appid=RJ45AJ-T8JV49E92P");
+		echo json_encode($return);
+	}
+	
+?>
